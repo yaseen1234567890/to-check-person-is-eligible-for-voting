a42daa280815017bbee87d758f725b71ea4a6201
yaseen1234567890 <112368880+yaseen1234567890@users.noreply.github.com>
2022-09-30 09:26:39 +0530

    Update yaseen2.cc

--- yaseen2.cc
+++ yaseen2.cc
@@ -1,25 +1,12 @@
 #include<iostream>
 using namespace std;
-
-
 int main()
 {
-	int age ;
-
-	
-	cout<<"Enter the age of the person: ";
-	cin>> age;
-
-	
-	if (age>=18)
-	{
-		cout<<"You are eligible for voting.";
-	}
-	else
-	{
-		cout<<"You are not eligible for voting.";
-	}
-
-	return 0;
+int a;
+cout<<"ENTER THE AGE => ";
+cin>>a;
+if(a>=18)
+cout<<"\nperson is eligible for voting ";
+if(a<18)
+cout<<(18-a)<<" years is left for voting => ";
 }
-
